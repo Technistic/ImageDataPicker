8de7af194fc9a25d4c9af3258d3506e9d38c6a41
Michael Logothetis <mmlogothetis@gmail.com>
2025-06-02 12:21:07 +1000

    Fix CI scripts to use $CI_WORKSPACE_PATH rather than $CI_WORKSPACE.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,22 +12,22 @@ echo "Building DocC documentation for ImageDataPicker"
 
 if [[ $CI_XCODEBUILD_ACTION = 'build' && $CI_PRODUCT_PLATFORM = 'iOS' ]];
 then
-xcodebuild -workspace $CI_WORKSPACE/ImageDataPicker.xcworkspace -derivedDataPath docsData -scheme EmployeeFormExample -destination 'platform=iOS Simulator,name=iPhone 16' -parallelizeTargets docbuild
-xcodebuild -workspace $CI_WORKSPACE/ImageDataPicker.xcworkspace -derivedDataPath docsData -scheme ImageDataPicker -destination 'platform=iOS Simulator,name=iPhone 16' -parallelizeTargets docbuild
+xcodebuild -workspace $CI_WORKSPACE_PATH/ImageDataPicker.xcworkspace -derivedDataPath docsData -scheme EmployeeFormExample -destination 'platform=iOS Simulator,name=iPhone 16' -parallelizeTargets docbuild
+xcodebuild -workspace $CI_WORKSPACE_PATH/ImageDataPicker.xcworkspace -derivedDataPath docsData -scheme ImageDataPicker -destination 'platform=iOS Simulator,name=iPhone 16' -parallelizeTargets docbuild
 
 echo "Copying DocC archives to doc_archives..."
-mkdir $CI_WORKSPACE/doc_archives
-cp -R `find docsData -type d -name "*.doccarchive"` $CI_WORKSPACE/doc_archives
+mkdir $CI_WORKSPACE_PATH/doc_archives
+cp -R `find docsData -type d -name "*.doccarchive"` $CI_WORKSPACE_PATH/doc_archives
 
-mkdir $CI_WORKSPACE/docs
+mkdir $CI_WORKSPACE_PATH/docs
 
-for ARCHIVE in $CI_WORKSPACE/doc_archives/*.doccarchive; do
+for ARCHIVE in $CI_WORKSPACE_PATH/doc_archives/*.doccarchive; do
     cmd() {
         echo "$ARCHIVE" | awk -F'.' '{print $1}' | awk -F'/' '{print tolower($2)}'
     }
     ARCHIVE_NAME="$(cmd)"
     echo "Processing Archive: $ARCHIVE"
-    $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path ImageDataPicker/$ARCHIVE_NAME --output-path $CI_WORKSPACE/docs/$ARCHIVE_NAME
+    $(xcrun --find docc) process-archive transform-for-static-hosting "$ARCHIVE" --hosting-base-path ImageDataPicker/$ARCHIVE_NAME --output-path $CI_WORKSPACE_PATH/docs/$ARCHIVE_NAME
 done
 
 ./ci_site_deploy.sh
