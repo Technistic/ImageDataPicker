4d77d81d4c6582c1b78b835dfc42df4c517ff371
Michael Logothetis <mmlogothetis@gmail.com>
2025-06-13 09:47:57 +1000

    Add alpha and beta documentation resources for CI.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,3 +5,25 @@
 #
 #  Created by Michael Logothetis on 26/5/2025.
 #  
+echo "Configuring build for $CI_BRANCH"
+
+if [[ "$CI_BRANCH" =~ ^(alpha|beta)?-?v([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+) ]]; then
+    major=${BASH_REMATCH[2]}
+    minor=${BASH_REMATCH[3]}
+    patch=${BASH_REMATCH[4]}
+    ver_type=${BASH_REMATCH[1]}
+
+    echo "Major: $major"
+    echo "Minor: $minor"
+    echo "Patch: $patch"
+    echo "Type: $ver_type"
+else
+    echo "Invalid Branch"
+    exit 1
+fi
+
+if [[ -n "$ver_type" ]]; then
+    echo "Copying $ver_type DocC Assets"
+    cp -R $ver_type/* $CI_WORKSPACE_PATH/ImageDataPicker/ImageDataPicker/ImageDataPicker.docc/Resources
+    cp -R $ver_type/* $CI_WORKSPACE_PATH/EmployeeFormExample/EmployeeFormExample/EmployeeFormExample.docc/Resources
+fi
