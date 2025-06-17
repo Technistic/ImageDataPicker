8b45d7b17f3e4ba46f730cc259bdb6882e393665
Michael Logothetis <mmlogothetis@gmail.com>
2025-06-17 14:27:30 +1000

    Add TestFligh "What to test" information for different archive types.

--- ci_scripts/testflight_test_info.sh
+++ ci_scripts/testflight_test_info.sh
@@ -5,8 +5,12 @@
 #
 #  Created by Michael Logothetis on 16/6/2025.
 #  
+echo "Setting up TestFlight test info"
+echo "CI_APP_STORE_SIGNED_APP_PATH: $CI_APP_STORE_SIGNED_APP_PATH"
+echo "CI_AD_HOC_SIGNED_APP_PATH: $CI_AD_HOC_SIGNED_APP_PATH"
+echo "CI_DEVELOPMENT_SIGNED_APP_PATH: $CI_DEVELOPMENT_SIGNED_APP_PATH"
 
-if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" || -d "$CI_AD_HOC_SIGNED_APP_PATH" || -d "$CI_DEVELOPMENT_SIGNED_APP_PATH" ]]; then
     TESTFLIGHT_DIR_PATH=../TestFlight
     mkdir -p $TESTFLIGHT_DIR_PATH
     git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
