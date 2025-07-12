e42a0b7b149813fe2d99dc7b55d09ca7802066f7
Michael Logothetis <mmlogothetis@gmail.com>
2025-07-13 08:26:41 +1000

    Remove debug info from check_draft_pr script.

--- ci_scripts/check_draft_pr.sh
+++ ci_scripts/check_draft_pr.sh
@@ -12,23 +12,22 @@ REPO="Technistic/ImageDataPicker"
 # If the workflow is triggered by a pull request, we can check if it is a draft PR.
 if [[ -n $CI_PULL_REQUEST_NUMBER ]]; then
     GITHUB_API="https://api.github.com/repos/$REPO/pulls/$CI_PULL_REQUEST_NUMBER"
+    echo "Checking if $GITHUB_API is a draft PR."
     
     # Fetch PR info using GitHub REST API
-    set +e
-    echo "$GITHUB_TOKEN"
-    RESPONSE=$(curl -vvv -S -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API")
-    echo "$RESPONSE"
+    RESPONSE=$(curl -S -H "Authorization: Bearer $GITHUB_TOKEN" "$GITHUB_API")
+
     # Parse the 'draft' field using jq (install or use inline grep if jq unavailable)
     IS_DRAFT_PR=$(echo "$RESPONSE" | grep -o '"draft": [^,]*' | awk '{print $2}')
 
-    echo "DRAFT PR: $IS_DRAFT"
+    if [[ -z "$IS_DRAFT_PR" ]]; then
+        echo "Could not retrieve PR $CI_PULL_REQUEST_NUMBER."
+        exit 0
+    fi
 
     # Exit workflow if this is a draft PR.
-    if [[ "$IS_DRAFT" = "true" ]]; then
+    if [[ "$IS_DRAFT_PR" = "true" ]]; then
         echo "Draft PR: Exiting Workflow."
         exit 0 # or exit with special code to skip later stages
     fi
-    
-    set -e
-    
 fi
