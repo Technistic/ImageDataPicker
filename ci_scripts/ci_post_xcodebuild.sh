06e11ec76604f9104ebbac3c5fa4cde976c9e429
Michael Logothetis <mmlogothetis@gmail.com>
2025-07-14 16:26:09 +1000

    Update CI workflows to manually publish integration, staging and rc branches.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -8,7 +8,7 @@
 
 
 #  Publish DocC documentation to GitHub Pages if there is a TAG change or manually triggered workflow.
-if [[ -n "$CI_TAG" || ("$CI_WORKFLOW" = "Publish-Pages-Integration" && ("$CI_START_CONDITION" = "manual" || "$CI_START_CONDITION" = "manual_rebuild")) ]]; then
+if [[ -n "$CI_TAG" || (("$CI_WORKFLOW" = "Publish-Pages-Integration" || "$CI_WORKFLOW" = "Publish-Pages-Staging" || "$CI_WORKFLOW" = "Publish-Pages-RC") && ("$CI_START_CONDITION" = "manual" || "$CI_START_CONDITION" = "manual_rebuild")) ]]; then
 
     #  Only publish GitHub Pages for iOS archive builds.
     if [[ ($CI_XCODEBUILD_ACTION = 'build' || $CI_XCODEBUILD_ACTION = 'archive') && $CI_PRODUCT_PLATFORM = 'iOS' ]]; then
