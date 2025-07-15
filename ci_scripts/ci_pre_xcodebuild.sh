a52c3be11d89e7dd79d62c08878b6c02292bb2a0
Michael Logothetis <mmlogothetis@gmail.com>
2025-07-16 09:39:45 +1000

    Change branch naming convention from release to production.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -12,7 +12,7 @@ echo "Configuring build for $CI_BRANCH"
 . ./get_version_from_git_ref.sh
 
 # Copy DocC assets for alpha and beta versions.
-if [[ -n "$ver_type" && "$ver_type" != "release" ]]; then
+if [[ -n "$ver_type" && "$ver_type" != "production" ]]; then
     echo "Copying $ver_type DocC Assets for alpha and beta versions."
     cp -R $ver_type/* ${CI_PRIMARY_REPOSITORY_PATH}/ImageDataPicker/ImageDataPicker/ImageDataPicker.docc/Resources
     cp -R $ver_type/* ${CI_PRIMARY_REPOSITORY_PATH}/EmployeeFormExample/EmployeeFormExample/EmployeeFormExample.docc/Resources
